d6d4d90ab538c0c4f629d5400a54cc5b50ccbc95
AchabNassim <achab.nassim.solicode@gmail.com>
2023-01-28 23:49:51 +0100

    more colors

--- script.js
+++ script.js
@@ -3,19 +3,23 @@ const ctx = canvas.getContext("2d");
 const images = [];
 let counter = 0;
 images[0] = new Image();
-images[0].src = "dvd1.png";
+images[0].src = "imgs/dvd1.png";
 images[1] = new Image();
-images[1].src = "dvd2.png";
+images[1].src = "imgs/dvd2.png";
 images[2] = new Image();
-images[2].src = "dvd3.png";
+images[2].src = "imgs/dvd3.png";
+images[3] = new Image();
+images[3].src = "imgs/dvd4.png";
+images[4] = new Image();
+images[4].src = "imgs/dvd5.png";
 
 const dvd = {
     sizeX : 100,
     sizeY : 70,
     x : (canvas.width / 2) - 100,
     y : (canvas.height / 2) - 100,
-    dx : 2.5,
-    dy : 2.5,
+    dx : 1.5,
+    dy : 1.5,
     index : 0
 };
 
@@ -37,7 +41,7 @@ function moveDvd() {
     {
         dvd.index++;
         counter = 0;
-        if (dvd.index > 2)
+        if (dvd.index > 4)
             dvd.index = 0;
     }
     ctx.drawImage(images[dvd.index], dvd.x, dvd.y);
