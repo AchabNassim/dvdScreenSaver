63989dd014ecf5fb5dda8055d067e6a320d96423
AchabNassim <achab.nassim.solicode@gmail.com>
2023-08-02 15:16:01 +0100

    responsive now.

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
+let counter = 0;
+const imagesNumber = 5;
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const images = [];
-let counter = 0;
-images[0] = new Image();
-images[0].src = "imgs/dvd1.png";
-images[1] = new Image();
-images[1].src = "imgs/dvd2.png";
-images[2] = new Image();
-images[2].src = "imgs/dvd3.png";
-images[3] = new Image();
-images[3].src = "imgs/dvd4.png";
-images[4] = new Image();
-images[4].src = "imgs/dvd5.png";
+const imagesSrc = [
+    "imgs/dvd1.png",
+    "imgs/dvd2.png",
+    "imgs/dvd3.png",
+    "imgs/dvd4.png",
+    "imgs/dvd5.png",
+];
+
+for (let i = 0; i < imagesNumber; i++) {
+    images[i] = new Image();
+    images[i].src = imagesSrc[i];
+}
 
 const dvd = {
-    sizeX : 100,
-    sizeY : 70,
-    x : (canvas.width / 2) - 100,
-    y : (canvas.height / 2) - 100,
+    sizeX : 98,
+    sizeY : 75,
+    x : (canvas.width - 98) / 2,
+    y : (canvas.height - 75) / 2,
     dx : 1.5,
     dy : 1.5,
     index : 0
@@ -26,17 +29,17 @@ const dvd = {
 function moveDvd() {
     dvd.x += dvd.dx;
     dvd.y += dvd.dy;    
-    if (dvd.x + dvd.sizeX > canvas.width || dvd.x < 0)
+    if (dvd.x + dvd.sizeX >= canvas.width || dvd.x < 0)
     {
         counter++;
         dvd.dx *= -1;        
     }
-    if (dvd.y + dvd.sizeY > canvas.height || dvd.y + 20 < 0)
+    if (dvd.y + dvd.sizeY >= canvas.height || dvd.y + 20 < 0)
     {
         counter++;
         dvd.dy *= -1;
     }
-    if (counter == 2)
+    if (counter == 1)
     {
         dvd.index++;
         counter = 0;
